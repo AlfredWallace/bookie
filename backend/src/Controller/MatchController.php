8927a70a851404a5d1e7904fad1dfd8b928c8c07
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2018-08-05 22:26:33 +0200

    Adapting the Match entity & controller to the new architecture (exporting some JMS serializer virtual properties in a service)

--- backend/src/Controller/MatchController.php
+++ backend/src/Controller/MatchController.php
@@ -2,91 +2,71 @@
 
 namespace App\Controller;
 
-use App\Entity\Bet;
-use App\Entity\Match;
-use App\Exception\ScoreFormatException;
 use App\Repository\MatchRepository;
-use App\Service\AlternativePointsCalculator;
-use App\Service\BasicPointsCalculator;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class MatchController extends ApiController
 {
-    const MATCH_NOT_FOUND_MESSAGE = 'Match not found.';
+    const MATCH_ROUTE = '/matches';
 
     /**
-     * @var SerializerInterface
-     */
-    private $serializer;
-
-    /**
-     * @var MatchRepository
-     */
-    private $matchRepository;
-
-    public function __construct(
-        SerializerInterface $serializer,
-        MatchRepository $matchRepository)
-    {
-        $this->serializer = $serializer;
-        $this->matchRepository = $matchRepository;
-    }
-
-    /**
-     * @Route("/matches", name="bookie_matches", methods={"GET"})
+     * @Route(
+     *     name="bookie_matches",
+     *     methods={"GET"}
+     * )
      *
+     * @param MatchRepository $matchRepository
+     * @return JsonResponse
      * @throws \Doctrine\ORM\Query\QueryException
      */
-    public function listMatches()
+    public function list(MatchRepository $matchRepository): JsonResponse
     {
-        return $this->matchRepository->findAllIndexedById();
+        return $this->getSerializedResponse($matchRepository->findAllIndexedById(), ['match.default']);
     }
 
-    /**
-     * @Route("/matches/{id}/end", name="bookie_matches_end", methods={"POST"})
-     * @Security("is_granted('ROLE_ADMIN')")
-     *
-     * @param Match $match
-     * @param Request $request
-     * @param BasicPointsCalculator $basicCalculator
-     * @param AlternativePointsCalculator $alternativeCalculator
-     * @return mixed
-     * @throws ScoreFormatException
-     */
-    public function endMatchAction(
-        Match $match,
-        Request $request,
-        BasicPointsCalculator $basicCalculator,
-        AlternativePointsCalculator $alternativeCalculator
-    )
-    {
-        $data = $request->getContent();
-        $scores = json_decode($data, true);
-
-        if ((isset($scores['home_score']) && $scores['home_score'] < 0)
-            || (isset($scores['away_score']) && $scores['away_score'] < 0)) {
-            throw new ScoreFormatException('Les scores ne peuvent pas être négatifs !');
-        }
-
-        $match->setIsOver(true);
-        $match->setHomeScore($scores['home_score']);
-        $match->setAwayScore($scores['away_score']);
-
-        /** @var Bet $bet */
-        foreach ($match->getBets() as $bet) {
-            $bet->setPoints($basicCalculator->getBetPoints($bet));
-            $bet->setPointsAlternative($alternativeCalculator->getBetPoints($bet));
-
-            $user = $bet->getUser();
-            $user->setPoints($basicCalculator->getUserPoints($user));
-            $user->setPointsAlternative($alternativeCalculator->getUserPoints($user));
-        }
-
-        $this->getDoctrine()->getManager()->flush();
-
-        return $match;
-    }
+//    /**
+//     * @Route("/matches/{id}/end", name="bookie_matches_end", methods={"POST"})
+//     * @Security("is_granted('ROLE_ADMIN')")
+//     *
+//     * @param Match $match
+//     * @param Request $request
+//     * @param BasicPointsCalculator $basicCalculator
+//     * @param AlternativePointsCalculator $alternativeCalculator
+//     * @return mixed
+//     * @throws ScoreFormatException
+//     */
+//    public function endMatchAction(
+//        Match $match,
+//        Request $request,
+//        BasicPointsCalculator $basicCalculator,
+//        AlternativePointsCalculator $alternativeCalculator
+//    )
+//    {
+//        $data = $request->getContent();
+//        $scores = json_decode($data, true);
+//
+//        if ((isset($scores['home_score']) && $scores['home_score'] < 0)
+//            || (isset($scores['away_score']) && $scores['away_score'] < 0)) {
+//            throw new ScoreFormatException('Les scores ne peuvent pas être négatifs !');
+//        }
+//
+//        $match->setIsOver(true);
+//        $match->setHomeScore($scores['home_score']);
+//        $match->setAwayScore($scores['away_score']);
+//
+//        /** @var Bet $bet */
+//        foreach ($match->getBets() as $bet) {
+//            $bet->setPoints($basicCalculator->getBetPoints($bet));
+//            $bet->setPointsAlternative($alternativeCalculator->getBetPoints($bet));
+//
+//            $user = $bet->getUser();
+//            $user->setPoints($basicCalculator->getUserPoints($user));
+//            $user->setPointsAlternative($alternativeCalculator->getUserPoints($user));
+//        }
+//
+//        $this->getDoctrine()->getManager()->flush();
+//
+//        return $match;
+//    }
 }
--- backend/src/Entity/Match.php
+++ backend/src/Entity/Match.php
@@ -9,89 +9,79 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * Class Match
- * @package App\Entity\Match
  * @ORM\Entity(repositoryClass="App\Repository\MatchRepository")
  * @ORM\Table(name="bookie_match")
  */
 class Match
 {
-    const MONTHS = [
-        'janvier',
-        'février',
-        'mars',
-        'avril',
-        'mai',
-        'juin',
-        'juillet',
-        'août',
-        'septembre',
-        'octobre',
-        'novembre',
-        'décembre',
-    ];
-    const DAYS = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi',];
-
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id()
      * @ORM\GeneratedValue(strategy="AUTO")
      *
-     * @var int
+     * @Groups({"match.default"})
      */
     protected $id;
 
     /**
      * @ORM\Column(type="boolean")
      *
-     * @var bool
+     * @Groups({"match.default"})
      */
     protected $isOver = false;
 
     /**
      * @ORM\Column(type="datetime", nullable=true)
-     * @Assert\DateTime()
      *
-     * @var \DateTime
+     * @Groups({"match.default"})
+     *
+     * @Assert\DateTime(message="Le coup d'envoi doit être une date au format AAAA-MM-JJ H:m:s")
      */
     protected $kickOff;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Team")
-     * @ORM\JoinColumn(nullable=false)
-     * @Groups({"match.teams"})
+     * @ORM\JoinColumn(nullable=true)
      *
-     * @var Team
+     * @Groups({"match.teams"})
      */
     protected $homeTeam;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Team")
-     * @ORM\JoinColumn(nullable=false)
-     * @Groups({"match.teams"})
+     * @ORM\JoinColumn(nullable=true)
      *
-     * @var Team
+     * @Groups({"match.teams"})
      */
     protected $awayTeam;
 
     /**
      * @ORM\Column(type="smallint", nullable=true)
-     * @Assert\GreaterThanOrEqual(value="0")
      *
-     * @var int
+     * @Groups({"match.default"})
+     *
+     * @Assert\GreaterThanOrEqual(
+     *     value="0",
+     *     message="Les scores doivent être des entiers positifs ou nuls"
+     * )
      */
-    protected $homeScore = 0;
+    protected $homeScore;
 
     /**
      * @ORM\Column(type="smallint", nullable=true)
-     * @Assert\GreaterThanOrEqual(value="0")
      *
-     * @var int
+     * @Groups({"match.default"})
+     *
+     * @Assert\GreaterThanOrEqual(
+     *     value="0",
+     *     message="Les scores doivent être des entiers positifs ou nuls"
+     * )
      */
-    protected $awayScore = 0;
+    protected $awayScore;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Bet", mappedBy="match")
+     *
      * @Groups({"match.bets"})
      */
     protected $bets;
@@ -101,187 +91,82 @@ class Match
         $this->bets = new ArrayCollection();
     }
 
-    /**
-     * @return bool
-     */
     public function isOver(): ?bool
     {
         return $this->isOver;
     }
 
-    /**
-     * @param mixed $isOver
-     * @return Match
-     */
     public function setIsOver(bool $isOver): self
     {
         $this->isOver = $isOver;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @Serializer\VirtualProperty()
-     *
-     * @return null|string
-     */
-    public function getPrettyKickOff(): ?string
-    {
-        $kickOff = $this->kickOff;
-
-        if ($kickOff !== null) {
-            $weekDay = self::DAYS[(int)$kickOff->format('w')];
-            $day = (int)$kickOff->format('j');
-            $dayText = $day === 1 ? $day . 'er' : $day;
-            $month = self::MONTHS[(int)$kickOff->format('n') - 1];
-            $year = $kickOff->format('Y');
-            return "$weekDay $dayText $month $year";
-        }
-
-        return null;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getKickOff(): ?\DateTime
     {
         return $this->kickOff;
     }
 
-    /**
-     * @param mixed $kickOff
-     * @return Match
-     */
     public function setKickOff(?\DateTime $kickOff): self
     {
         $this->kickOff = $kickOff;
         return $this;
     }
 
-    /**
-     * @Serializer\VirtualProperty()
-     *
-     * @return int|null
-     */
-    public function getHomeTeamId(): ?int
-    {
-        return isset($this->homeTeam) ? $this->homeTeam->getId() : null;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getHomeTeam(): ?Team
     {
         return $this->homeTeam;
     }
 
-    /**
-     * @param mixed $homeTeam
-     * @return Match
-     */
     public function setHomeTeam(?Team $homeTeam): self
     {
         $this->homeTeam = $homeTeam;
         return $this;
     }
 
-    /**
-     * @Serializer\VirtualProperty()
-     *
-     * @return int|null
-     */
-    public function getAwayTeamId(): ?int
-    {
-        return isset($this->awayTeam) ? $this->awayTeam->getId() : null;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getAwayTeam(): ?Team
     {
         return $this->awayTeam;
     }
 
-    /**
-     * @param mixed $awayTeam
-     * @return Match
-     */
     public function setAwayTeam(?Team $awayTeam): self
     {
         $this->awayTeam = $awayTeam;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getHomeScore(): ?int
     {
         return $this->homeScore;
     }
 
-    /**
-     * @param mixed $homeScore
-     * @return Match
-     */
     public function setHomeScore(int $homeScore): self
     {
         $this->homeScore = $homeScore;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
     public function getAwayScore(): ?int
     {
         return $this->awayScore;
     }
 
-    /**
-     * @param mixed $awayScore
-     * @return Match
-     */
     public function setAwayScore(int $awayScore): self
     {
         $this->awayScore = $awayScore;
         return $this;
     }
 
-    /**
-     * @Serializer\VirtualProperty()
-     *
-     * @return array
-     */
-    public function getBetsIds(): ?array
-    {
-        return array_map(function (Bet $bet) {
-            return $bet->getId();
-        }, $this->bets->toArray());
-    }
-
-    /**
-     * @return mixed
-     */
     public function getBets(): ?Collection
     {
         return $this->bets;
     }
 
-    /**
-     * @param Bet $bet
-     * @return $this
-     */
     public function addBet(Bet $bet): self
     {
         if (!$this->bets->contains($bet)) {
@@ -291,36 +176,9 @@ class Match
         return $this;
     }
 
-    /**
-     * @param Bet $bet
-     * @return $this
-     */
     public function removeBet(Bet $bet): self
     {
-        if ($this->bets->contains($bet)) {
-            $this->bets->removeElement($bet);
-            if ($bet->getMatch() === $this) {
-                $bet->setMatch(null);
-            }
-        }
+        $this->bets->removeElement($bet);
         return $this;
-    }
-
-    /**
-     * @Serializer\VirtualProperty()
-     *
-     * @return bool
-     */
-    public function isToday(): ?bool
-    {
-        $kickOff = $this->kickOff;
-        if ($kickOff === null) {
-            return false;
-        }
-
-        $today = new \DateTime();
-        return (int)$today->format('Y') === (int)$kickOff->format('Y')
-            && (int)$today->format('n') === (int)$kickOff->format('n')
-            && (int)$today->format('j') === (int)$kickOff->format('j');
     }
 }
