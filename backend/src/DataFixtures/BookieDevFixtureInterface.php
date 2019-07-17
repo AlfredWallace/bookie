9a4b8a9872960ab978ca1cfe8eb5d8c637caad2a
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2019-07-17 22:43:27 +0200

    /!\ /!\ /!\ Trashing the backend /!\ /!\ /!\

--- backend/src/DataFixtures/BookieDevFixtureInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\DataFixtures;
-
-interface BookieDevFixtureInterface
-{
-}
--- backend/src/Tests/DataFixtures/BookieTestFixtureInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Tests\DataFixtures;
-
-interface BookieTestFixtureInterface
-{
-}
